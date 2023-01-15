896c41b61c89fccfc32d2bf7de860ad0469bbb78
Florian Kreiner <fkreiner@mbp-effy.fritz.box>
2023-01-15 23:04:06 +0100

    auto reconnect and mac address interface

--- src/xwifi.h
+++ src/xwifi.h
@@ -13,6 +13,7 @@ public:
     void begin();
     void loop();
     void getStatus(wl_status_t& oStatus);
+    String getMac();
 private:
     wl_status_t mStatus = WL_NO_SHIELD;
     bool mDisplayRequest = false;
@@ -29,8 +30,10 @@ inline XWiFi::XWiFi(const char *iSsid, const char *iPassphrase)
 
 inline void XWiFi::begin()
 {
+
     WiFi.begin(mSsid, mPassphrase);
     esp_err_t wifi_success = esp_wifi_set_ps(WIFI_PS_NONE);
+
     if (wifi_success == ESP_OK)
     {
         X_INFO("WiFi set to no powersaving");
@@ -39,6 +42,7 @@ inline void XWiFi::begin()
     {
         X_INFO("Error when trying to set WiFi to no powersaving");
     }
+    WiFi.setAutoReconnect(true);
 }
 
 inline void XWiFi::loop()
@@ -50,9 +54,21 @@ inline void XWiFi::loop()
         X_DEBUG("Wifi State changed");
         mDisplayRequest = true;
     }
+
+    //reconnect if disconnected
 }
 
 inline void XWiFi::getStatus(wl_status_t& oStatus)
 {
     oStatus = mStatus;
+}
+
+inline String XWiFi::getMac()
+{
+    byte mac[6];
+    WiFi.macAddress(mac);
+    char macBuffer[13];
+    sprintf(macBuffer, "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    return String(macBuffer);
+
 }
